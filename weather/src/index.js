499492bff74da346ad7661c25ce043a8580308a1
Phuc-Dao <tidaotidao@yahoo.com>
2018-06-08 13:04:36 -0700

    build: added api fetch and style

--- weather/src/index.js
+++ weather/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import Form from './componant/Form'
 
 ReactDOM.render(<App /> , document.getElementById('root'));
